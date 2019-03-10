585a88223eb1188b9ba8b75c7b561c70284eea2d
aomine <xomarxpl07@outlook.es>
2019-03-10 12:16:00 -0500

    v 1.3

--- js/app.js
+++ js/app.js
@@ -1,26 +1,21 @@
-const wall = document.querySelector('.wall'),
-x = document.querySelector('.x'),
-icon = document.querySelector('.icon'),
-nav = document.getElementById('nav');
-icon.addEventListener('click', () => {
-	icon.classList.toggle('active');
-	nav.classList.toggle('active');
-	wall.classList.remove('wall-none');
-	wall.classList.remove('wall-effect');
-	wall.classList.toggle('x');
-	if (!wall.classList.contains('x')) {
-		vanished()
-	}
-});
-wall.addEventListener('click', vanished);
-function vanished(){
-	wall.classList.add('wall-effect');
-	nav.classList.remove('active');
-	icon.classList.remove('active');
-	wall.classList.remove('x');
-	setTimeout(none, 500);
-}
-function none(){
-	wall.classList.add('wall-none');
-	wall.classList.remove('wall-effect');
-}
+		const header = document.querySelector('header'),
+		icon = document.querySelector('.icon'),
+		wall = document.querySelector('.wall');
+		icon.addEventListener('click', ()=>{
+			header.classList.toggle('active');
+			if (wall.classList.contains('wall-none')) {
+				wall.classList.remove('wall-none');
+			}else{
+				wall.classList.add('wall-effect');
+				setTimeout(none, 500);
+			}
+		});
+		wall.addEventListener('click', ()=>{
+			header.classList.remove('active');
+			wall.classList.add('wall-effect');
+			setTimeout(none, 500);
+		});
+		function none(){
+			wall.classList.remove('wall-effect');
+			wall.classList.add('wall-none');
+		}
